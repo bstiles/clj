878b91d74845a0ee8c0b7eabab5aa214ef69e8a4
Brian Stiles <bstiles@bstiles.net>
2011-02-25 06:44:25 -0800

    Renamed variable.

--- src/net/bstiles/clj/env/rhino.clj
+++ src/net/bstiles/clj/env/rhino.clj
@@ -4,6 +4,6 @@
 (defn make-exec-fn
   [& args]
   (fn [class-loader]
-    (let [jruby-main (.. class-loader (loadClass "org.mozilla.javascript.tools.shell.Main"))
+    (let [rhino-main (.. class-loader (loadClass "org.mozilla.javascript.tools.shell.Main"))
           args (into-array String args)]
-      (call-method jruby-main :main [(class args)] nil args))))
+      (call-method rhino-main :main [(class args)] nil args))))
